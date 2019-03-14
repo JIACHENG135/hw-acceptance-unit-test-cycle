f83b76e0550ecd6f8490b231d6b562e9cd0db194
JIACHENG135 <liulaoye135@tamu.edu>
2019-03-14 23:14:26 +0000

    try

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -2,6 +2,7 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
+    
   end
 
   def show
--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -22,7 +22,7 @@
 #     step %{I should see "#{movie.title}"}
 #   end
 # end
-Given /the following movies exist:$/ do |movies_table|
+Given /the following movies exist/ do |movies_table|
     movies_table.hashes.each do |hash|
         Movie.create!(title: hash['title'], rating: hash['rating'], release_date: hash['release_date'], director: hash['director'])
     end
